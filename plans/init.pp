dabc84846d113bf1848dcc94989d865e641b7a27
Reid Vandewiele <reid@puppetlabs.com>
2019-05-24 11:01:17 -0600

    Clean up plan and comment docs

--- plans/init.pp
+++ plans/init.pp
@@ -1,38 +1,39 @@
 # Intended to allow this to work:
 #
-# $series_results = run_plan('task_series',
-#   nodes => $targets,
-#   tasks => [
-#     [ 'util::linux_patching',
-#         foo => bar,
-#         boo => baz,
-#     ],
-#     [ 'util::reboot',
-#         blort => zlot,
-#     ],
-#     [ 'util::validate',
-#         arg => val,
-#     ],
-#   ])
+#     $series_results = run_plan('task_series',
+#       nodes => $targets,
+#       tasks => [
+#         [ 'util::linux_patching',
+#             foo => bar,
+#             boo => baz,
+#         ],
+#         [ 'util::reboot',
+#             blort => zlot,
+#         ],
+#         [ 'util::validate',
+#             arg => val,
+#         ],
+#       ])
 #
 # And return a Hash that looks like:
 #
-# {
-#   "results" => [
-#     ResultSet,
-#     ResultSet,
-#     ResultSet,
-#   ],
-#   "summary" => {
-#     "errored-at-step-2": [
-#       "local://1",
-#       "local://2",
-#     ],
-#     "succeeded": [
-#       "local://3",
-#     ],
-#   },
-# }
+#     {
+#       "result_sets" => [
+#         ResultSet,
+#         ResultSet,
+#         ResultSet,
+#         ResultSet,
+#       ],
+#       "summary" => {
+#         "errored at step 2: task::name": [
+#           Target.name,
+#           Target.name,
+#         ],
+#         "succeeded": [
+#           Target.name,
+#         ],
+#       },
+#     }
 #
 plan task_series (
   TargetSpec  $nodes,
@@ -40,32 +41,29 @@ plan task_series (
 ) {
   $targets = get_targets($nodes)
 
-  # Short-circuit the plan if there are no targets
-  # TODO: is this even necessary?
-  if $targets.empty { return({'results' => ResultSet([]), 'summary' => {}}) }
-
   # Filter out targets not currently reachable
   $connection_results = wait_until_available($targets,
     wait_time     => 0,
     _catch_errors => true,
   )
 
-  $result_sets = $tasks.reduce([$connection_results]) |$results_array, $task| {
+  $task_results = $tasks.reduce([$connection_results]) |$results, $task| {
     $task_array = Array($task)
     notice($task_array)
-    $results_array << run_task($task_array[0], $results_array[-1].ok_set.targets,
+    $results << run_task($task_array[0], $results[-1].ok_set.targets,
       $task_array[1] + { _catch_errors => true },
     )
-  }
+  }[1,-1] # omit the connection results from $task_results
 
   $indexed_labeled_results = [
-    [0, 'validate connection', $result_sets[0]]
-  ] + $result_sets[1,-1].map |$index, $result| {
+    [0, 'validate connection', $connection_results]
+  ] + $task_results.map |$index, $result| {
     [$index + 1, $tasks[$index][0], $result]
   }
 
   return({
-    'result_sets' => $result_sets,
-    'summary' => task_series::summarize($indexed_labeled_results),
+    'connection_results' => $connection_results,
+    'task_results'       => $task_results,
+    'summary'            => task_series::summarize($indexed_labeled_results),
   })
 }
