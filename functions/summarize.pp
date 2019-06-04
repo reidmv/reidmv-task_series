8070c83f91999a0475a6322ec55ab2efb77ac16e
Reid Vandewiele <reid@puppetlabs.com>
2019-06-04 16:50:52 -0700

    Add comments and clean up variable names
    This makes it easier to understand what's going on.
    
    Using the more common variable name in the reduce function "memo", for
    "memory", to store previous iteration's results.

--- functions/summarize.pp
+++ functions/summarize.pp
@@ -1,10 +1,15 @@
 function task_series::summarize(
-  Array[Tuple[Integer, String, ResultSet]] $indexed_labeled_results,
+  Array[Tuple[Integer, String, ResultSet]] $index_label_results,
 ) {
-  $summary_struct = $indexed_labeled_results.reduce([]) |$array, $tup| {
-    $array << ["errored at step ${tup[0]}: ${tup[1]}", $tup[2].error_set.names]
-  } << ['succeeded', $indexed_labeled_results[-1][2].ok_set.names]
+  # Build an array consisting of one entry per step, with the list of nodes
+  # that failed at that step. Finalize it by adding, at the end, the list of
+  # nodes that did not fail at any step (the nodes that succeeded).
+  $summary_struct = $index_label_results.reduce([]) |$memo, $tuple| {
+    $memo << ["errored at step ${tuple[0]}: ${tuple[1]}", $tuple[2].error_set.names]
+  } << ['succeeded', $index_label_results[-1][2].ok_set.names]
 
+  # Remove from the array any steps at which no nodes failed. Turn the filtered
+  # summary into a hash.
   $summary = Hash($summary_struct).filter |$key, $value| { ! $value.empty }
 
   return($summary)
--- plans/init.pp
+++ plans/init.pp
@@ -47,15 +47,26 @@ plan task_series (
     _catch_errors => true,
   )
 
-  $task_results = $tasks.reduce([$connection_results]) |$results, $task| {
-    $task_array = Array($task)
-    notice($task_array)
-    $results << run_task($task_array[0], $results[-1].ok_set.targets,
-      $task_array[1] + { _catch_errors => true },
+  # Run all tasks in the series, one after the other, collecting each resultset
+  # in an array. When running each task, only run it on the targets that
+  # successfully completed the previous task.
+  $task_results = $tasks.reduce([$connection_results]) |$memo, $tuple| {
+    $title  = $tuple[0]
+    $params = $tuple[1]
+
+    # Run the task and add its results to memo. The targets are the nodes which
+    # successfully completed the previous task.
+    $memo << run_task($title, $memo[-1].ok_set.targets,
+      $params + { _catch_errors => true },
     )
+
   }[1,-1] # omit the connection results from $task_results
 
-  $indexed_labeled_results = [
+  # Build an array of [INDEX, NAME, RESULTS] tuples. This will be passed into
+  # the task_series::summarize function later to produce a readable summary of
+  # which nodes succeeded, which nodes failed, and for the failed nodes, which
+  # step they failed at.
+  $index_label_results = [
     [0, 'validate connection', $connection_results]
   ] + $task_results.map |$index, $result| {
     [$index + 1, $tasks[$index][0], $result]
@@ -64,6 +75,6 @@ plan task_series (
   return({
     'connection_results' => $connection_results,
     'task_results'       => $task_results,
-    'summary'            => task_series::summarize($indexed_labeled_results),
+    'summary'            => task_series::summarize($index_label_results),
   })
 }
